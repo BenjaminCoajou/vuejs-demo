822096de51ee4e5d30bb161f6c3349a76a899b70
Benjamin Coajou <coajou.benjamin@gmail.com>
2021-06-02 09:05:27 +0200

    api liste de course & call the api

---
+++ api/migrations/Version20210601132553.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210601132553 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, buy TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE personne');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE personne (id INT AUTO_INCREMENT NOT NULL, prenom VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, nom VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE item');
+    }
+}
---
+++ api/src/Controller/ItemController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Item;
+use App\Repository\ItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ItemController extends AbstractController
+{
+    /**
+     * @Route("/items/",
+     *  name="api_item_displayAll", methods={"GET"})
+     */
+    public function displayAll(ItemRepository $repo): Response
+    {
+        return $this->json($repo->findAll());
+    }
+    /**
+     * @Route("/items/", name="api_item_add", methods={"POST"})
+     */
+    public function addItem(Request $request, EntityManagerInterface $em)
+    {
+        // objet json item
+       $item = json_decode($request->getContent());
+
+       $newItem = new Item();
+       $newItem->setName($item->name);
+       $newItem->setBuy(false);
+
+       $em->persist($newItem);
+       $em->flush();
+
+       $tab["message"] = "Hello Ajouter ! " . $item->name;
+       return $this->json($tab);
+    }
+    /**
+     * @Route("/items/{id}", name="api_item_remove", methods={"DELETE"})
+     */
+    public function removeItem(Item $item, EntityManagerInterface $em)
+    {
+        $em->remove($item);
+        $em->flush();
+        $tab["message"] = "supprimer";
+        return $this->json($tab);
+    }
+    /**
+     * @Route("/items/{id}", name="api_item_update", methods={"PUT"})
+     */
+    public function updateItem(Request $request, Item $item, EntityManagerInterface $em)
+    {
+        $i = json_decode($request->getContent());
+
+        $item->setName($i->name);
+
+        $em->persist($item);
+        $em->flush();
+    }
+}
